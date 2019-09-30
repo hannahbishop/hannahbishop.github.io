4e25a3b2464a0585f6cedd1b2d1097cccb89c6c2
Hannah Bishop <hannahbananabishop@gmail.com>
2019-09-29 22:26:44 -0700

    create header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <div className="header">
+      <a className="btn-link" href="/">Resume</a>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+function Home() {
+  return(
+    <div className="main-content">
+      <Header/>
+      <p>heyooo</p>
+    </div>
+  )
+}
+
+export default Home;
