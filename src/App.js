1e43a24b9fa1793670097e0cfc1f6f20d971db43
Hannah Bishop <hannahbananabishop@gmail.com>
2019-10-01 15:23:36 -0700

    Resolve remaining merge conflicts
    Thanks Visual Studio >.<

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
 import Helmet from 'react-helmet';
-=======
->>>>>>> 4e25a3b2464a0585f6cedd1b2d1097cccb89c6c2
 import './scss/main.scss';
 import Home from './Home.js';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
         <Helmet>
           <link rel="stylesheet" href="https://use.typekit.net/xfu3ysz.css"/>
         </Helmet>
-=======
->>>>>>> 4e25a3b2464a0585f6cedd1b2d1097cccb89c6c2
         <Home ></Home>
     </div>
   );
