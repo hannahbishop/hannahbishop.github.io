39dcd86f71a5cbfbefb3b935b1a54b154ce45b3a
Hannah Bishop <hannahbananabishop@gmail.com>
2019-10-01 15:12:23 -0700

    Build homepage
    This is only a one page site! But in the future it could have more
    pages, so let's name appropriately.

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import TitleCard from './TitleCard';
+
+function Home() {
+  return(
+    <div>
+      <Header/>
+      <TitleCard/>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/TitleCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TitleCard() {
+  return(
+    <div className="title-card">
+      <h1>Hannah Bishop</h1>
+      <h2>Software Developer</h2>
+      <a className="btn-link" href="/">Résumé</a>
+    </div>
+  )
+}
+
+export default TitleCard;
