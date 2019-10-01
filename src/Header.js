1fcf50bb237624b9090bb2c7c754474177f78434
Hannah Bishop <hannahbananabishop@gmail.com>
2019-10-01 15:10:49 -0700

    Build header
    The header only contains 3 social links right now.

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <div className="header">
+      <ul className="socials-list">
+        <li><a href="https://github.com/hannahbishop">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/hannah-bishop-9a9916137/">LinkedIn</a></li>
+        <li><a href="mailto:hbishop@uvic.ca">Email</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header;
