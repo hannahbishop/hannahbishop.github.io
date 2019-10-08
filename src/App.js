160911a9a51f68d4d81323b266b4767bb428779c
Hannah Bishop <hannahbananabishop@gmail.com>
2019-10-08 16:43:35 -0700

    Move font link to index.html

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 import './scss/main.scss';
 import Home from './Home.js';
 
 function App() {
   return (
     <div className="App">
-        <Helmet>
-          <link rel="stylesheet" href="https://use.typekit.net/xfu3ysz.css"/>
-        </Helmet>
         <Home ></Home>
     </div>
   );
