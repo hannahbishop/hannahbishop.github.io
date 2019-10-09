24a3e0189ea050899b302f372cb3cb7573c18c00
Hannah Bishop <hannahbananabishop@gmail.com>
2019-10-08 19:19:43 -0700

    Add illustration and make things mobile-snazzay

--- src/TitleCard.js
+++ src/TitleCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { ReactComponent as Illustration } from './me.svg';
 
 function TitleCard() {
   return(
     <div className="title-card">
-      <h1>Hannah Bishop</h1>
-      <h2>Software Developer</h2>
-      <a className="btn-link" href="/">Résumé</a>
+      <div className="title-card__text">
+        <h1>Hannah Bishop</h1>
+        <h2>Software Engineer</h2>
+        <a className="btn-link" href="/">Résumé</a>
+      </div>
+      <Illustration className="illustration"/>
     </div>
   )
 }
