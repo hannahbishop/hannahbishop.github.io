d02c1da1fb9edfdb4c32daffb61f81a4951e8817
Hannah Bishop <hannahbananabishop@gmail.com>
2019-10-08 17:01:03 -0700

    Updates

---
+++ precache-manifest.2af29b4b1fdbeb631ecc34541df9107a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bbee37ee507f24082f9aac2b241295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b592120f079bcca2e79",
+    "url": "/static/css/main.8ade5ef0.chunk.css"
+  },
+  {
+    "revision": "1c1d2aec03c9bbef731f",
+    "url": "/static/js/2.6e84c7e3.chunk.js"
+  },
+  {
+    "revision": "0b592120f079bcca2e79",
+    "url": "/static/js/main.9969d47a.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  }
+]);
