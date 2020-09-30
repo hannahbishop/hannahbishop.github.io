aa5bedc3b5695acb512e039f12e35111746f721f
hannahbishop <hannahbananabishop@gmail.com>
2020-09-30 15:31:30 -0700

    Update 2020

---
+++ build/precache-manifest.fadc6a01c1a0a15fd18049eba5e68506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d18c6d0adc426e488cd4846c40fd81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e10d4909ca3dbae8db14",
+    "url": "/static/css/main.3845f606.chunk.css"
+  },
+  {
+    "revision": "1c1d2aec03c9bbef731f",
+    "url": "/static/js/2.6e84c7e3.chunk.js"
+  },
+  {
+    "revision": "e10d4909ca3dbae8db14",
+    "url": "/static/js/main.e8e6c63b.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  },
+  {
+    "revision": "ebccbfc29a35b7f4497163e460aec18c",
+    "url": "/static/media/me.ebccbfc2.svg"
+  }
+]);
